95dff5508ebcb1854f282e217c83c2e889e10608
HojinAn <asdv94@naver.com>
2022-03-11 02:14:01 +0900

    :sparkles: 회원가입 페이지 기능

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,13 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import clayful from "clayful/client-js";
 
 function RegisterPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const Customer = clayful.Customer;
+
+    const payload = {
+      email,
+      password,
+    };
+    console.log("payload", payload);
+
+    Customer.createMe(payload, function (err, result) {
+      if (err) {
+        // Error Case
+        console.log(err.code);
+      }
+
+      navigate("/login");
+    });
+  };
+
   return (
     <div className="auth-wrapper">
       <h1>회원가입</h1>
-      <form>
-        <input placeholder="Apple ID" type="email" name="email" value="" />
-        <input placeholder="암호" type="password" name="password" value="" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          placeholder="Apple ID"
+          type="email"
+          name="email"
+          value={email}
+        />
+        <input
+          onChange={handlePasswordChange}
+          placeholder="암호"
+          type="password"
+          name="password"
+          value={password}
+        />
 
         <button type="submit">회원가입</button>
         <Link to="/login" style={{ color: "gray", textDecoration: "none" }}>

d526ee2532f1cb8f91fe7fa468b94f220ad4f2ab
HojinAn <asdv94@naver.com>
2022-03-14 01:36:02 +0900

    :sparkles: 로그인 페이지 기능 구현

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,70 @@
-import React from "react";
+import clayful from "clayful/client-js";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const Customer = clayful.Customer;
+    const payload = {
+      // email: email,
+      // password: password,
+      // key, value가 같으면 그냥 하나로 써도 된다.
+      email,
+      password,
+    };
+    Customer.authenticate(payload, function (err, result) {
+      if (err) {
+        // Error case
+        console.log(err.code);
+        return;
+      }
+      const data = result.data;
+      localStorage.setItem("customerUid", data.customer);
+      localStorage.setItem("accessToken", data.token);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="auth-wrapper">
+      <h1>로그인</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          placeholder="Apple ID"
+          type="email"
+          name="email"
+          value={email}
+        />
+        <input
+          onChange={handlePasswordChange}
+          placeholder="암호"
+          type="password"
+          name="password"
+          value={password}
+        />
+        <p>Apple ID는 iTunes, App Store, iCloud에 로그인할 때 사용하는 이메일 주소 입니다.</p>
+
+        <button type="submit">로그인</button>
+        <Link to="/register" style={{ color: "gray", textDecoration: "none" }}>
+          Apple ID가 없으신가요? 지금 생성
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;

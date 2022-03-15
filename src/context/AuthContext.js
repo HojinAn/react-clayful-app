3f0e22f8d57d1f94dbf97c13462dad0b37f855ab
HojinAn <asdv94@naver.com>
2022-03-15 23:07:33 +0900

    :passport_control: AuthContext를 통한 로그인 상태 유지 기능 구현

---
+++ src/context/AuthContext.js
@@ -0,0 +1,51 @@
+import clayful from "clayful/client-js";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [isAuth, setIsAuth] = useState(false);
+
+  const isAuthenticated = () => {
+    const Customer = clayful.Customer;
+
+    const options = {
+      customer: localStorage.getItem("accessToken"),
+    };
+
+    Customer.isAuthenticated(options, function (err, result) {
+      if (err) {
+        // Error case
+        console.log(err.code);
+        // Error 났을 때는 일단 false
+        setIsAuth(false);
+        return;
+      }
+
+      const data = result.data;
+
+      if (data.authenticated) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+    });
+  };
+
+  const signOut = () => {
+    setIsAuth(false);
+    localStorage.removeItem("accessToken");
+    navigate("/login");
+  };
+
+  const AuthContextData = {
+    isAuth,
+    isAuthenticated,
+    signOut,
+  };
+  return <AuthContext.Provider value={AuthContextData}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContextProvider;

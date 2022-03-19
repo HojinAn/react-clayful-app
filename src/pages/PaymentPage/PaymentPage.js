19dd24cde65f2cfc814ecbeebff97aad60eefff6
HojinAn <asdv94@naver.com>
2022-03-19 19:54:08 +0900

    :lipstick: 결제 페이지 UI 구현

--- src/pages/PaymentPage/PaymentPage.js
+++ src/pages/PaymentPage/PaymentPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import clayful from "clayful/client-js";
+import "./PaymentPage.css";
 function PaymentPage() {
   const [cart, setCart] = useState({});
   const [paymentMethods, setPaymentMethods] = useState([]);
@@ -43,7 +44,53 @@ function PaymentPage() {
     });
   };
 
-  return <div>PaymentPage</div>;
+  return (
+    <div className="pageWrapper">
+      <div className="payment">
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            borderBottom: "1px solid #d2d2d7",
+          }}
+        >
+          <div style={{ width: "50%", fontSize: 24, fontWeight: 500 }}> 결제</div>
+          <div style={{ width: "50%", display: "flex", justifyContent: "end" }}>
+            주문 총 가격: {cart.total?.amount.raw + 3000}원 (3000원 배송비)
+          </div>
+        </div>
+        <div style={{ marginTop: 16, width: "100%", display: "flex" }}>
+          <div style={{ width: "49%" }}>
+            <h5>주문자 정보</h5>
+            <input type="text" name="full" placeholder="주문자명" />
+            <input type="text" name="mobile" placeholder="무선처연락처" />
+            <div>
+              <input type="checkbox" id="sameInfo" name="sameInfo" />
+              <label htmlFor="sameInfo">수취자 정보도 위와 동일합니다.</label>
+            </div>
+          </div>
+          <div style={{ width: "2%" }} />
+          <div style={{ width: "49%" }}>
+            <h5>수취자 정보</h5>
+            <input type="text" name="full" placeholder="수취자명" />
+            <input type="text" name="mobile" placeholder="무선처연락처" />
+
+            <h5>배송 정보</h5>
+            <input type="text" readOnly placeholder="주소" />
+            <input type="text" name="address2" placeholder="상세 주소" />
+            <input type="text" readOnly placeholder="우편번호" />
+
+            <h5>결제</h5>
+            <select>
+              <option>옵션</option>
+            </select>
+
+            <button style={{ width: "100%", marginTop: 10 }}>주문</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PaymentPage;
